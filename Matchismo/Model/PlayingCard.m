a6315cfcf7d22a90a05f0a020e0d6d9cd4c79109
Rob Freiburger <rob@robfreiburger.com>
2013-02-09 15:37:22 -0800

    Overwrites match method to support multiple cards.

--- Matchismo/Model/PlayingCard.m
+++ Matchismo/Model/PlayingCard.m
@@ -45,4 +45,21 @@
 	return [self rankStrings].count - 1;
 }
 
+// Overwritting match implementation
+- (int)match:(NSArray *)otherCards {
+	int score = 0;
+	
+	if (otherCards.count == 1) {
+		PlayingCard *otherCard = [otherCards lastObject];
+		
+		if ([otherCard.suit isEqualToString:self.suit]) {
+			score = 1;
+		} else if (otherCard.rank == self.rank) {
+			score = 4;
+		}
+	}
+	
+	return score;
+}
+
 @end
