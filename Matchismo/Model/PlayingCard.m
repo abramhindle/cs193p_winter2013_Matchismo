34b1665f36cc2d621d12cea433a67340c6ef2ebd
Rob Freiburger <rob@robfreiburger.com>
2013-02-11 16:59:02 -0800

    Fixes possible crash with PlayingCard match method.

--- Matchismo/Model/PlayingCard.m
+++ Matchismo/Model/PlayingCard.m
@@ -50,12 +50,17 @@
 	int score = 0;
 	
 	if (otherCards.count == 1) {
-		PlayingCard *otherCard = [otherCards lastObject];
 		
-		if ([otherCard.suit isEqualToString:self.suit]) {
-			score = 1;
-		} else if (otherCard.rank == self.rank) {
-			score = 4;
+		id otherCard = [otherCards lastObject];
+		
+		if ([otherCard isKindOfClass:[PlayingCard class]]) {
+			PlayingCard *playingCard = (PlayingCard *)otherCard;
+			
+			if ([playingCard.suit isEqualToString:self.suit]) {
+				score = 1;
+			} else if (playingCard.rank == self.rank) {
+				score = 4;
+			}
 		}
 	}
 	
